e54e374e2aa479f80bf43553e6e3af34458b2a50
Mu Yuan <yuanmu.email@gmail.com>
2023-07-10 22:24:32 +0800

    Add files via upload

--- 1.c
+++ 1.c
@@ -1,10 +1,53 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-    float r,c,s;
-    scanf("%f",&r);
-    c=2*3.415926*r;
-    s=3.1415926*r*r;
-    printf("周长=%.2f\n面积=%.2f",c,s);
+    int m,n,i,j;
+    scanf("%d",&m);
+    int arr1[100]={0};
+    int arr2[100]={0};
+    int fangjian[100]={23333};
+    int count=0;
+    for(i=0;i<m;i++)
+        scanf("%d",&arr1[i]);
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        scanf("%d",&arr2[i]);
+    for(i=0;i<sizeof(arr1)/sizeof(arr1[0]);i++)
+    {
+        for(j=0;j<sizeof(arr2)/sizeof(arr2[0]);j++)
+        {
+            if(arr1[i]==arr2[j])
+            {
+                fangjian[count]=arr1[i];
+                count++;
+                break;
+            }
+        }
+    }
+    for(i=0;i<count-1;i++)
+    {
+        for(j=i+1;j<count;j++)
+        {
+            if(fangjian[i]==fangjian[j])
+            {
+                fangjian[j]=114514;
+            }
+        }
+    }
+    printf("%d\n",count);
+    if(fangjian[0]==23333)
+    {
+        printf("No Answer");
+    }
+    else
+    {
+        for(i=0;i<count;i++)
+        {
+            if(fangjian[i]!=114514)
+            {
+                printf("%d ",fangjian[i]);
+            }
+        }
+    }
     return 0;
 }
